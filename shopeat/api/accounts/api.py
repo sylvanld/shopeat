8d727e28a6bd3ff802e1b47f8a4ef835796c94a0
Sylvan Le Deunff <sylvan.le-deunff@ovhcloud.com>
2022-12-22 20:24:33 +0100

    Add ability to get token both from json payload or form data

--- shopeat/api/accounts/api.py
+++ shopeat/api/accounts/api.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter, Depends
+import json
+from typing import Optional
+from urllib.parse import parse_qsl
+
+from fastapi import APIRouter, Depends, Header, HTTPException, Request
 
 from shopeat.core.auth.fastapi import authenticate
 from shopeat.core.auth.tokens import generate_token
@@ -15,8 +19,31 @@ router = APIRouter(tags=["Accounts"])
 account_service = AccountService(SQLAccountRepository(DATABASE))
 
 
+async def auth_credentials(request: Request):
+    """Dependency used to accept credentials either from form data or json payload!"""
+
+    content_type = request.headers.get("Content-Type", "application/json")
+
+    if content_type == "application/x-www-form-urlencoded":
+        raw_body = (await request.body()).decode("utf-8")
+        request_data = dict(parse_qsl(raw_body))
+    elif content_type == "application/json":
+        request_data = await request.json()
+    else:
+        raise HTTPException(400, detail="Unsupported content type!")
+
+    if request_data.get("username") is None or request_data.get("password") is None:
+        raise HTTPException(
+            400,
+            "You must provide both username and password as form data, or json payload!",
+            headers={"X-Received-Data": json.dumps(request_data)},
+        )
+
+    return CredentialsDTO.parse_obj(request_data)
+
+
 @router.post("/auth/token")
-async def get_token(credentials: CredentialsDTO):
+async def get_token(credentials: CredentialsDTO = Depends(auth_credentials)):
     account = await account_service.authenticate(credentials)
     return {"access_token": generate_token(account.uid)}
 
